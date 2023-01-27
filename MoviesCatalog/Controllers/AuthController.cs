34eb6577f743273c8004f72a039fb57197b60521
Ilia Volgin <228133713372282004@stud.tsu.ru>
2023-01-27 17:09:59 +0700

    Add project files.

---
+++ MoviesCatalog/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesCatalog.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public string Register()
+        {
+            return "Registration succeded";
+        }
+        [HttpPost]
+        public string Login()
+        {
+            return "Logged in";
+        }
+        [HttpPost]
+        public string Logout()
+        {
+            return "Logged out";
+        }
+    }
+}
