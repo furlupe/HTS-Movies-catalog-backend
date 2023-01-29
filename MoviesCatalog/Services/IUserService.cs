e286ff8e6338cf9beec6b2bb2e4ccfa7b7d86faa
Ilia Volgin <228133713372282004@stud.tsu.ru>
2023-01-29 14:28:06 +0700

    Described profile DTO, profile service & controller

---
+++ MoviesCatalog/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using MoviesCatalog.Models.DTO;
+
+namespace MoviesCatalog.Services
+{
+    public interface IUserService
+    {
+        Task<UserProfileDto> GetProfile(Guid id);
+        Task UpdateProfile(UserProfileDto profile);
+    }
+}
---
+++ MoviesCatalog/Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿using MoviesCatalog.Models.DTO;
+
+namespace MoviesCatalog.Services
+{
+    public class UserService : IUserService
+    {
+        public Task<UserProfileDto> GetProfile(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProfile(UserProfileDto profile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
